22555bce75edb9b294f5009f65e194bfd951aff7
mailinsz <mailinsonez@gmail.com>
2018-06-28 20:01:40 -0300

    Cambios en App.js

--- movierant/src/App.js
+++ movierant/src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './images/tomatito.png';
 import './App.css';
+import { TomatesTruchosRoutes } from './routes'
+import { BrowserRouter } from 'react-router-dom'
+
 
 class App extends Component {
   render() {
@@ -10,11 +13,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Tomates Truchos</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <TomatesTruchosRoutes />
+        <BrowserRouter/>
       </div>
-    );
+    )
   }
 }
 
